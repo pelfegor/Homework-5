a7e6f5bf8abd3fb06feb513f18bf3b88dcece2a1
Levon Poghosyan <levon9393@bk.ru>
2022-07-17 18:22:31 +0300

    Tabulation

--- src/main/java/ru/netology/sqr/services/SQRService.java
+++ src/main/java/ru/netology/sqr/services/SQRService.java
@@ -3,10 +3,10 @@ package ru.netology.sqr.services;
 public class SQRService {
     public int amountNumbersWithinBoundaries(int lowerBound, int upperBound) {
         int amount = 0;
-        for(int i=10;i<=99;i++) {
+        for (int i = 10; i <= 99; i++) {
 
-            if(i * i >= lowerBound && i * i <= upperBound) {
-                 amount++;
+            if (i * i >= lowerBound && i * i <= upperBound) {
+                amount++;
             }
         }
         return amount;
